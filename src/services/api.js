4970ad533e69d48be4027bd2e296e5179bd8ff69
gci07 <iit2022124@iiita.ac.in>
2025-06-13 05:22:27 +0530

    gg

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "74e2f530dc2043c61539510219328bdf";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
